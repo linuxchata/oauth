a3a2facf6076f5edf7d410c60ca680a9f8208556
linuxchata <linuxchata@gmail.com>
2024-10-23 21:06:10 +0200

    Improve token request

--- src/Shark.AuthorizationServer/Requests/TokenRequest.cs
+++ src/Shark.AuthorizationServer/Requests/TokenRequest.cs
@@ -4,20 +4,28 @@ namespace Shark.AuthorizationServer.Requests;
 
 public sealed class TokenRequest
 {
+    [DefaultValue("")]
     public string? grant_type { get; set; }
 
+    [DefaultValue("")]
     public string? code { get; set; }
 
+    [DefaultValue("")]
     public string? code_verifier { get; set; }
 
+    [DefaultValue("")]
     public string? redirect_uri { get; set; }
 
+    [DefaultValue("")]
     public string? client_id { get; set; }
 
+    [DefaultValue("")]
     public string? client_secret { get; set; }
 
+    [DefaultValue("")]
     public string? scope { get; set; }
 
+    [DefaultValue("")]
     public string? refresh_token { get; set; }
 
     [DefaultValue("")]
