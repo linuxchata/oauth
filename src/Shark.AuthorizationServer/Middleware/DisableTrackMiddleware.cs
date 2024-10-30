9222573cb1db3490dbb30c8843394325ddfe04d2
linuxchata <linuxchata@gmail.com>
2024-10-30 20:50:35 +0100

    Add DisableTrackMiddleware

---
+++ src/Shark.AuthorizationServer/Middleware/DisableTrackMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Shark.AuthorizationServer.Common.Extensions;
+
+namespace Shark.AuthorizationServer.Middleware;
+public class DisableTrackMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Method.EqualsTo("TRACE") ||
+            context.Request.Method.EqualsTo("OPTIONS"))
+        {
+            context.Response.StatusCode = StatusCodes.Status405MethodNotAllowed;
+            await context.Response.WriteAsync("Method Not Allowed");
+            return;
+        }
+
+        await _next(context);
+    }
+}
